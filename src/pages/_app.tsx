86450e1d98a133486463107f02417c685a69f41a
SaltyAom <aomkirby123@gmail.com>
2021-05-10 20:25:33 +0700

    :wrench: fix - theme not matching navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -30,7 +30,7 @@ const App = ({ Component, pageProps }: AppProps) => {
             <Head>
                 <meta
                     name="theme-color"
-                    content={isDarkTheme ? '#1f2937' : '#ffffff'}
+                    content={isDarkTheme ? '#374151' : '#ffffff'}
                 />
             </Head>
             <Provider>
